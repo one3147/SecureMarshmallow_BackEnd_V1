0a1b895c8e5f6389cea43b89b384cd67edc5aab5
junha <goodjunha@gmail.com>
2023-05-01 17:49:19 +0900

    🔨:: #9 어노테이션 추가
    테스트 동시에 나오는 것을 막는 AutoConfigureTestDatabase 구현

--- SecureMarshmallo_BackEnd_V1/src/test/java/com/securemarshmallo/securemarshmallo_backend_v1/repository/JapRepositoryTest.java
+++ SecureMarshmallo_BackEnd_V1/src/test/java/com/securemarshmallo/securemarshmallo_backend_v1/repository/JapRepositoryTest.java
@@ -6,12 +6,17 @@ import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("testdb")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
