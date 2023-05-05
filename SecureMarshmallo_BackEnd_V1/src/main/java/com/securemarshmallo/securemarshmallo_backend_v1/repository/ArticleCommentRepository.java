94ccebe4b163a2a246bcd026a6827af1904895bf
junha <goodjunha@gmail.com>
2023-05-05 11:55:31 +0900

    🌊 :: #4 RepositoryRestResource 추가

--- SecureMarshmallo_BackEnd_V1/src/main/java/com/securemarshmallo/securemarshmallo_backend_v1/repository/ArticleCommentRepository.java
+++ SecureMarshmallo_BackEnd_V1/src/main/java/com/securemarshmallo/securemarshmallo_backend_v1/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.securemarshmallo.securemarshmallo_backend_v1.repository;
 
 import com.securemarshmallo.securemarshmallo_backend_v1.domain.article.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
